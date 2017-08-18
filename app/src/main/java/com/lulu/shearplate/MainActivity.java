f7441da4c7fcc58efbd24d9617fa5ef54930fd57
zhanglulu <zhanglulu@foxmail.com>
2017-08-18 14:41:29 +0800

    将郭霖的代码先copy过来，实现悬浮窗

--- app/src/main/java/com/lulu/shearplate/MainActivity.java
+++ app/src/main/java/com/lulu/shearplate/MainActivity.java
@@ -1,7 +1,12 @@
 package com.lulu.shearplate;
 
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Button startFloatWindow = (Button) findViewById(R.id.start_float_window);
+        startFloatWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (Settings.canDrawOverlays(MainActivity.this)) {
+                        Intent intent = new Intent(MainActivity.this, FloatWindowService.class);
+                        startService(intent);
+                        finish();
+                    } else {
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                        startActivity(intent);
+                    }
+                } else {
+                    Intent intent = new Intent(MainActivity.this, FloatWindowService.class);
+                    startService(intent);
+                    finish();
+                }
+            }
+        });
     }
 }
